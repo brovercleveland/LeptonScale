d798071f98831c80a2ad89506515ba614789ac88
Brian Pollack <brian.pollack@cern.ch>
2014-07-11 16:59:16 -0500

    first commit

---
+++ rootlogon.C
@@ -0,0 +1,14 @@
+{     
+  // include path for RooFit
+  TString rfitpath("$ROOFITSYS/include");
+  TString path = gSystem->GetIncludePath();
+  path += "-I. -I$ROOTSYS/src -I";
+  path += rfitpath;
+  gSystem->SetIncludePath(path.Data());
+
+  gROOT->Macro("CPlot.cc++");
+  gROOT->Macro("MitStyleRemix.cc++");
+  	    
+  // Show which process needs debugging
+  gInterpreter->ProcessLine(".! ps |grep root.exe");  
+}
